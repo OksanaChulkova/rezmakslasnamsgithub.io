356c43a0e66e4352e841c81731cd528123ea84bf
OksanaChulkova <124584081+OksanaChulkova@users.noreply.github.com>
2024-04-21 23:58:04 +0300

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,10 @@
+{
+  "name": "rezeknes-makslas-nams",
+  "version": "1.0.0",
+  "private": true,
+  "dependencies": {
+    "parcel-bundler": "^1.6.1"
+  },
+  "scripts": {},
+  "devDependencies": {}
+}
